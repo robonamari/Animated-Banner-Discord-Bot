59c0939245028d48fb7899da790f1d81499ccd2c
robonamari <ROBONAMARI@GMAIL.COM>
2024-07-24 07:42:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import requests
 import base64
 
-token = ''
-banner_path = './Animated-Banner.gif'
+token = ""
+banner_path = "./Animated-Banner.gif"
 
 try:
-    with open(banner_path, 'rb') as file:
-        new_banner = base64.b64encode(file.read()).decode('utf-8')
+    with open(banner_path, "rb") as file:
+        new_banner = base64.b64encode(file.read()).decode("utf-8")
 
     headers = {
-        'Authorization': f'Bot {token}',
-        'Content-Type': 'application/json'
+        "Authorization": f"Bot {token}",
+        "Content-Type": "application/json"
     }
 
-    data = {
-        'banner': f'data:image/gif;base64,{new_banner}'
+    json = {
+        "banner": f"data:image/gif;base64,{new_banner}"
     }
 
-    response = requests.patch('https://discord.com/api/v10/users/@me', headers=headers, json=data)
+    response = requests.patch("https://discord.com/api/v10/users/@me", headers=headers, json=json)
 
     if response.ok:
-        print('Banner Updated!')
+        print("Banner Updated!")
     else:
-        print('Failed to Update Banner:', response.status_code)
-        print('Response body:', response.text)
+        print("Failed to Update Banner:", response.status_code)
+        print("Response body:", response.text)
 
 except Exception as error:
-    print('There is an Error here:', error)
+    print("There is an Error here:", error)
